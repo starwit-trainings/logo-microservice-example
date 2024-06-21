bdcc85f41a0506d4e2a5d9f13a8ceae59f2d00c6
ztarbug <kronprinzfriedrich@gmx.de>
2024-06-21 14:06:58 +0200

    initial commit

---
+++ Readme.md
@@ -0,0 +1,5 @@
+# Sample micro service in Python
+
+This repository contains a simple REST service, that is used to explain micro services. Goal is to have a plain version and one secured with API access tokens.
+
+Service is defined with an openAPI spec that can be found [here](api-definition/logoservice.yaml).
---
+++ logo-service/main.py
@@ -0,0 +1,62 @@
+from __future__ import annotations
+from typing import List
+from datetime import datetime
+
+import uvicorn
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
+
+from models import Logo
+from models import Info
+
+app = FastAPI(
+    title='Logo service',
+    version='0.0.1',
+    description='This is a sample service, that provides logo images.\n',
+    servers=[{'url': 'http://localhost:8080/v0'}],
+)
+
+data = [{"name": "starwit",
+         "creator": "Markus",
+         "imageUri": "resources/starwit.png"},
+        {"name": "starwit-bw",
+         "creator": "Markus",
+         "imageUri": "resources/starwit-bw.png"}]
+
+@app.get('/info', response_model=None)
+def get_info() -> Info:
+    info = Info()
+    info.generation_date = datetime.now()
+    info.systemDescription = "a sample service"
+    info.apiVersion = "0.0.1"
+    return info
+
+@app.get('/logo/all', response_model=List[Logo])
+def get_all_logos() -> List[Logo]:
+    result = []
+    for idx, d in enumerate(data):
+        l = Logo()
+        l.id = idx
+        l.name = d['name']
+        l.creator = d['creator']
+        l.imageUri = d['imageUri']
+        result.append(l)
+    return result
+
+@app.get('/logo/{id}', response_model=Logo)
+def get_logo_by_id(id: int) -> Logo:
+    d = data[id]
+    l = Logo()
+    l.id = id
+    l.name = d['name']
+    l.creator = d['creator']
+    l.imageUri = d['imageUri']
+    return l
+
+@app.get('/logo/image/{id}', response_model=bytes)
+def get_logo_image(id: int) -> bytes:
+    fr = FileResponse(data[id]['imageUri'])
+    return fr
+
+if __name__ == '__main__':
+    uvicorn.run('main:app', host="127.0.0.1", port=8000)
